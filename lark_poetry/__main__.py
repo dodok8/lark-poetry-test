d22abf628460bf32c122e1bb8fc5e4ce1d201a28
Kim, Hyeonseo <dodok8@gmail.com>
2022-11-04 16:10:36 +0900

    Add hello, world example

---
+++ lark_poetry/__main__.py
@@ -0,0 +1,8 @@
+from lark import Lark
+
+l = Lark('''start: WORD "," WORD "!"
+            %import common.WORD   // imports from terminal library
+            %ignore " "           // Disregard spaces in text
+        ''')
+
+print(l.parse("Hello, World!"))
