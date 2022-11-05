8cfd4666deb7df2f9f1ccb0d7336709c48d89f12
Kim, Hyeonseo <dodok8@gmail.com>
2022-11-05 14:12:14 +0900

    Add simple grammar

--- lark_poetry/__main__.py
+++ lark_poetry/__main__.py
@@ -5,4 +5,8 @@ l = Lark('''start: WORD "," WORD "!"
             %ignore " "           // Disregard spaces in text
         ''')
 
+print(__file__)
+eql = Lark.open('grammar/grammar.lark', rel_to=__file__, parser="lalr")
+
 print(l.parse("Hello, World!"))
+print(eql.parse("create"))
